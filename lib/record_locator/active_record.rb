ff34feb432499be15f823e01cbf25182109d469a
we4tech <hasan83bd@gmail.com>
2013-11-17 02:23:59 +0600

    Eagerly load active_record/extension module.

--- lib/record_locator/active_record.rb
+++ lib/record_locator/active_record.rb
@@ -1,6 +1,7 @@
 module RecordLocator
   module ActiveRecord
     autload :Finder     'record_locator/active_record/finder'
-    autload :Extension  'record_locator/active_record/extension'
   end
 end
+
+require 'record_locator/active_record/extension'
